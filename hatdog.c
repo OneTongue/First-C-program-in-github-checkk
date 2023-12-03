c7bc8b6ba2290ccea511476b037ceef9c5dc72cc
OneTongue <152796393+OneTongue@users.noreply.github.com>
2023-12-04 00:01:41 +0800

    Update hatdog.c

--- hatdog.c
+++ hatdog.c
@@ -7,8 +7,13 @@ int joshlee(int x, int y){
 int main(){
   int x, y;
   
-  printf("Hello World");
-  joshlee(x, y);
+  printf("Enter first number: ");
+  scanf("%d", &x);
+  printf("Enter second number: ");
+  scanf("%d", &y);
+  
+  printf("The sum is %d", joshlee(x, y);
+  
 
   return 0;
 }
